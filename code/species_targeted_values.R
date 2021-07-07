5e26f84c28e962f85635492db715230fbe95ebb0
MihirTakalkar <papyarocks@gmail.com>
2021-07-07 12:09:06 -0700

    Finished graphs for top species targeted by value.

--- code/species_targeted_values.R
+++ code/species_targeted_values.R
@@ -26,13 +26,13 @@ artdata <- mexicoLandings %>%
   filter(year %in% years) %>%
   filter(fishery_type == "Artisanal") %>%
   # filter(level != "species") %>%
-  select(year, fishery_type, sci_name, landings_kg) %>%
+  select(year, fishery_type, sci_name, value_mxn) %>%
   # Group by Fishery Type and Sort #
   group_by(year, sci_name) %>%
-    # Calculate Sums #
-    summarize(landings_kg_tot = sum(landings_kg))
-  # Pick Top Species #
-  artdata <- artdata[order(-artdata$landings_kg_tot),] %>%
+  # Calculate Sums #
+  summarize(value_mxn_tot=sum(value_mxn))
+# Pick Top Species #
+artdata <- artdata[order(-artdata$value_mxn_tot),] %>%
   slice(1:5)
 
 inddata <- mexicoLandings %>%
@@ -40,39 +40,39 @@ inddata <- mexicoLandings %>%
   filter(year %in% years) %>%
   filter(fishery_type == "Industrial") %>%
   # filter(level != "species") %>%
-  select(year, fishery_type, sci_name, landings_kg) %>%
+  select(year, fishery_type, sci_name, value_mxn) %>%
   # Group by Fishery Type and Sort #
   group_by(year, sci_name) %>%
   # Calculate Sums #
-  summarize(landings_kg_tot = sum(landings_kg))
-  # Pick Top Species #
-  inddata <- inddata[order(-inddata$landings_kg_tot),] %>%
+  summarize(value_mxn_tot=sum(value_mxn))
+# Pick Top Species #
+inddata <- inddata[order(-inddata$value_mxn_tot),] %>%
   slice(1:5)
 
 view(artdata)
 view(inddata)
 
 # Plot Artisanal Data #
-gart <- ggplot(data = artdata, aes(x = year, y = landings_kg_tot / 1000, fill = sci_name)) +
+gart <- ggplot(data = artdata, aes(x = year, y = value_mxn_tot / 10e6, fill = sci_name)) +
   geom_col() +
   labs(title = "Species Targeted by Artisanal Fisheries",
-       subtitle = "What are the top five species targeted annually by artisanal fisheries based on landings?",
-       x ="Year", y = "Landings (Metric Tons)", fill = "Scientific Name") +
+       subtitle = "What are the top five species targeted annually by artisanal fisheries based on revenue?",
+       x ="Year", y = "Value in Pesos (Nillions)", fill = "Scientific Name") +
   theme_fivethirtyeight() +
   theme(axis.title = element_text(), text = element_text(size = 10, family = "Segoe UI"))
-  # Save #
-  ggsave(gart, filename=file.path(plotdir, "artisanal_species_landings.png"),
+# Save #
+ggsave(gart, filename=file.path(plotdir, "artisanal_species_value.png"),
        units="in", width=10, height=8.0, dpi=600)
 
 # Plot Industrial Data #
-gind <- ggplot(data = inddata, aes(x = year, y = landings_kg_tot / 1000, fill = sci_name)) +
+gind <- ggplot(data = inddata, aes(x = year, y = value_mxn_tot / 10e6, fill = sci_name)) +
   geom_col() +
   labs(title = "Species Targeted by Industrial Fisheries",
-       subtitle = "What are the top five species targeted annually by industrial fisheries based on landings?",
-       x ="Year", y = "Landings (Metric Tons)", fill = "Scientific Name") +
+       subtitle = "What are the top five species targeted annually by industrial fisheries based on revenue?",
+       x ="Year", y = "Value in Pesos (Millions)", fill = "Scientific Name") +
   theme_fivethirtyeight() +
   theme(axis.title = element_text(), text = element_text(size = 10, family = "Segoe UI"))
 
-  # Save #
-  ggsave(gind, filename=file.path(plotdir, "industrial_species_landings.png"),
-         units="in", width=10, height=8.0, dpi=600)
+# Save #
+ggsave(gind, filename=file.path(plotdir, "industrial_species_value.png"),
+       units="in", width=10, height=8.0, dpi=600)
