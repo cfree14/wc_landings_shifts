20b42df277c2162ef0339e0fcf2875c79d999fec
MihirTakalkar <papyarocks@gmail.com>
2021-07-07 00:16:01 -0700

    Continued work on weighted barplot for targeted species.

--- code/species_targeted.R
+++ code/species_targeted.R
@@ -21,17 +21,45 @@ summary(mexicoLandings)
 years <- 2001:2019
 
 # Data Build #
-data <- mexicoLandings %>%
+artdata <- mexicoLandings %>%
   # Eliminate Columns and Years #
   filter(year %in% years) %>%
-  filter(fishery_type == "Artisinal") %>%
-  filter(level != "species") %>%
-  select(year, fishery_type, sci_name, level, landings_kg, value_mxn) %>%
+  filter(fishery_type == "Artisanal") %>%
+  # filter(level != "species") %>%
+  select(year, fishery_type, sci_name, landings_kg, value_mxn) %>%
   # Group by Fishery Type and Sort #
-  group_by(year, fishery_type) %>%
+  group_by(year, sci_name) %>%
     # Calculate Sums #
     summarize(landings_kg_tot=sum(landings_kg),
-              value_mxn_tot=sum(value_mxn) / 10e6)
+              value_mxn_tot=sum(value_mxn))
+  # Pick Top Species #
+  artdata <- artdata[order(-artdata$landings_kg_tot, -artdata$value_mxn_tot),] %>%
+  slice(1:10)
 
-view(data)
+inddata <- mexicoLandings %>%
+  # Eliminate Columns and Years #
+  filter(year %in% years) %>%
+  filter(fishery_type == "Industrial") %>%
+  # filter(level != "species") %>%
+  select(year, fishery_type, sci_name, landings_kg, value_mxn) %>%
+  # Group by Fishery Type and Sort #
+  group_by(sci_name) %>%
+  # Calculate Sums #
+  summarize(landings_kg_tot=sum(landings_kg),
+            value_mxn_tot=sum(value_mxn))
+  # Pick Top Species #
+  inddata <- inddata[order(-inddata$landings_kg_tot, -inddata$value_mxn_tot),] %>%
+  slice(1:5)
+
+# Plot Data #
+ggplot(data = inddata, aes(x = sci_name)) +
+  geom_bar() +
+  labs(title = "Species Targeted by Artisinal Fisheries",
+       subtitle = "What types of species and groups do artisinal fisheries target?",
+       x ="Year", y = "Proportion of Landings", color = "Species")
+
+
+
+view(artdata)
+view(inddata)
 
