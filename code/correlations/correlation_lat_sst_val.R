4c8c29c379719b57142a49511e4ae00c39112384
Chris Free <cfree14@gmail.com>
2021-07-23 16:26:25 -0700

    added new figure

--- code/correlations/correlation_lat_sst_val.R
+++ code/correlations/correlation_lat_sst_val.R
@@ -32,7 +32,7 @@ data <- mexdata %>%
 
 sdata <- data %>%
   # Filter to species, region, and years of interest
-  filter(sci_name == "Haemulon sp") %>%
+  filter(sci_name == "Sardinops sagax") %>%
   filter(year %in% years) %>%
   # Calculate total annual revenues by office
   group_by(year, office, ecoregion, lat_dd) %>%
@@ -40,7 +40,8 @@ sdata <- data %>%
   ungroup() %>%
   # Calculate revenue-weighted average latitude
   group_by(year) %>%
-  summarize(lat_dd=weighted.mean(x=lat_dd, w=landings_kg)) %>%
+  summarize(lat_dd=weighted.mean(x=lat_dd, w=landings_kg),
+            landings_kg=sum(landings_kg)) %>%
   ungroup()
 
   # Create SST Data #
@@ -72,15 +73,46 @@ sdata <- data %>%
     ungroup()
 
 
+# New plot for paper
+g <- ggplot(sdata2, aes(x=lat_dd, y=landings_kg/1000/1000, fill=sst_c)) +
+  geom_point(pch=21, size=3) +
+  geom_smooth(method="lm", color="black", fill=NA) +
+  # Limits
+  lims(y=c(0,NA)) +
+  # Labels
+  labs(x="Latitude (°N)", y="Landings (1000s mt)") +
+  # Legend
+  scale_fill_gradientn(name="SST (°C)", colors=rev(RColorBrewer::brewer.pal(9, "RdBu"))) +
+  guides(fill = guide_colorbar(ticks.colour = "black", frame.colour = "black")) +
+  # Theme
+  theme_bw() +
+  theme(axis.text=element_text(size=6),
+         axis.title=element_text(size=8),
+         legend.text=element_text(size=6),
+         legend.title=element_text(size=8),
+         strip.text=element_text(size=8),
+         plot.title=element_text(size=10),
+         # Gridlines
+         panel.grid.major = element_blank(),
+         panel.grid.minor = element_blank(),
+         panel.background = element_blank(),
+         axis.line = element_line(colour = "black"))
+g
+
+# Export plot
+ggsave(g, filename=file.path(plotdir, "sardine_landings_vs_lat_by_sst.png"),
+       width=4.5, height=4, units="in", dpi=600)
 
 
 # Plot Data #
-  # SST and Mean Latitude #
-  g3 <- ggplot(sdata2, aes(x=sst_c, y=lat_dd)) +
-        geom_point() +
-        geom_smooth(method="lm") +
-        labs(x="SST (°C)", y="Latitude (°N)") +
-        theme_bw()
+# SST and Mean Latitude #
+g3 <- ggplot(sdata2, aes(x=sst_c, y=lat_dd)) +
+      geom_point() +
+      geom_smooth(method="lm") +
+      labs(x="SST (°C)", y="Latitude (°N)") +
+      theme_bw()
+g3
+
   # Pearson Correlation #
   # Mean Latitude and Landings #
   g4 <- ggplot(sdata3, aes(x=lat_dd, y=landings_kg/1000/1000)) +
@@ -88,6 +120,7 @@ sdata <- data %>%
         geom_smooth(method="lm") +
         labs(x="Landings-weighted latitude (°N)", y="Landings (1000s mt)") +
         theme_bw()
+  g4
   # Pearson Correlation #
 
 # Combine Graphs #
